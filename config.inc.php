2df476c6a9a4f6551083de8596488228f4aa0c1c
AMKuperus <jezeewolde@gmail.com>
2017-03-16 11:15:44 +0100

    Changed config

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,13 @@
 <?php
+// 1.) Fill in all the empty fields with their appropriate settings inbetween ''.
+// 2.) Rename this file to config.inc
+
+  //Server name (like 'localhost' if the server is localhost).
   $server = 'localhost';
+  //Database name.
   $dbname = 'sat';
+  //Username (database user with basic read-write acces)
   $user = 'itvitae';
-  $pass = 'NekN7nQA37xtB13U';
+  //Password of the Database-user
+  $pass = 'YHIBYhB2N5UcyAJQ';
 ?>
