3eb1bb2179ed42028bf69ccada709e357d4f70b1
AMKuperus <jezeewolde@gmail.com>
2017-04-11 14:32:09 +0200

    SAT login

--- login.inc.php
+++ login.inc.php
@@ -22,13 +22,31 @@
 
   require_once 'jumper.inc.php';
   include 'functions.inc.php';
-
+//TODO fix this this is noy working, createHash function creates deifferent results
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(isset($_POST['userName']) && isset($_POST['pass'])) {
       $username = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_STRING);
-      $password = createHash($_POST['pass']);
+      //$password = createHash($_POST['pass']);
+      $password = $_POST['pass'];
+      $hashpw = createHash($password);
+      $pc = logUser($db, $username);
+      print_r($pc);
+      $ppc = implode($pc);
+      //TODO Check if the passCode's match
+      if(password_verify($password, $ppc)) {
+        echo "<h1>CHECK</h1>";
+      } else {
+        echo "dang....";
+      }
+      $one = createHash($password);
+      $two = createHash($password);
 
-      print_r(logUser($db, $username));
+      echo "<div class=box style=z-index:999><br><code>input: $password</code>
+            <br><code>hash: $hashpw</code>
+            <br><code>DB: $ppc</code><br>
+            <br>$one
+            <br>$two</div>";
+
     } else {
       //warning for not filling in field
     }
