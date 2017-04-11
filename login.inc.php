ebc8b921317697fde25a18061e2ab00249108be1
AMKuperus <jezeewolde@gmail.com>
2017-04-11 16:02:26 +0200

    SAT login
    + fixed login to verify password correclty after fixing bug in
    createHash()

--- login.inc.php
+++ login.inc.php
@@ -22,30 +22,18 @@
 
   require_once 'jumper.inc.php';
   include 'functions.inc.php';
-//TODO fix this this is noy working, createHash function creates deifferent results
   if($_SERVER['REQUEST_METHOD'] == 'POST') {
     if(isset($_POST['userName']) && isset($_POST['pass'])) {
       $username = filter_input(INPUT_POST, 'userName', FILTER_SANITIZE_STRING);
-      //$password = createHash($_POST['pass']);
       $password = $_POST['pass'];
-      $hashpw = createHash($password);
-      $pc = logUser($db, $username);
-      print_r($pc);
-      $ppc = implode($pc);
-      //TODO Check if the passCode's match
-      if(password_verify($password, $ppc)) {
+      //TODO let db function return string so we do not need implode
+      $pc = implode(logUser($db, $username));
+      // Check if the passCode's match
+      if(password_verify($password, $pc)) {
         echo "<h1>CHECK</h1>";
       } else {
         echo "dang....";
       }
-      $one = createHash($password);
-      $two = createHash($password);
-
-      echo "<div class=box style=z-index:999><br><code>input: $password</code>
-            <br><code>hash: $hashpw</code>
-            <br><code>DB: $ppc</code><br>
-            <br>$one
-            <br>$two</div>";
 
     } else {
       //warning for not filling in field
