4a0d6081fcd96262f8037660d4ac926f965311be
guidoleijten <guido.leijten@itvitaelearning.nl>
2017-04-19 12:30:50 +0200

    Update to admin

--- admin.class.php
+++ admin.class.php
@@ -1,12 +1,12 @@
 <?php
 class Admin extends User {
 
-  public function callUpdateRole() {
+  public function callUpdateRole($db) {
     $storage = new Storage($db);
     $update = $storage->updateRole();
   }
   public function removeUser() {
-    
+
   }
 }
 ?>
